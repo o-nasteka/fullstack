978bf1f944d1c5e6567e21bb5f41eef5e762fc6a
Oleg Nasteka <oleg.nasteka@gmail.com>
2018-08-06 11:42:22 +0300

    Create Category

--- controllers/order.js
+++ controllers/order.js
@@ -1,7 +1,22 @@
+const Order = require('../models/Order')
+const errorHandler = require('../utils/errorHandler')
+
 module.exports.getAll = function(req, res) {
 
 }
 
-module.exports.create = function(req, res) {
+module.exports.create = async function(req, res) {
+    try {
 
+
+        const order = await new Order({
+            list: req.body.list,
+            user: req.user.id,
+            order: maxOrder + 1
+        }).save()
+
+        res.status(201).json(order)
+    } catch (e) {
+        errorHandler(res, e)
+    }
 }
